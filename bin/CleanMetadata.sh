d725aba752f5fe1b43f7f544e44534fb6886fc81
Larysha <60649802+Larysha@users.noreply.github.com>
2022-02-08 07:55:53 +0200

    Update CleanMetadata.sh

--- bin/CleanMetadata.sh
+++ bin/CleanMetadata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# For each cell line (HMEC, MCF7 and MDA-MB-231) and antigen type (HM and TF) 
+# Keep only the relevant metadata columns after filtering and remove the "%20" spacer and 
+# replace with a single splace using sed
+
 cut --complement -f 9-14 hisHMECanno.tsv | sed 's/%20/ /g' > hisHMECannoFilt.tsv
 cut --complement -f 9-14 hisMCF7anno.tsv | sed 's/%20/ /g' > hisMCF7annoFilt.tsv
 cut --complement -f 9-14 hisMDAMB231anno.tsv | sed 's/%20/ /g' > hisMDAMB231annoFilt.tsv
